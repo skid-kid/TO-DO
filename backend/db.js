5d9adaeafacd5534c08e2cd635ee8f71c59aea46
skid-kid <polarb587@gmail.com>
2024-07-16 16:45:05 +0530

    fixed bug

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
-mongoose.connect("mongodb+srv://huehue:12345678910@cluster0.pxzhocv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect("mongodb+srv://@cluster0.pxzhocv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 const todoSchema=mongoose.Schema({
     title:String,
     description: String,
